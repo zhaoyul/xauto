d01f5357ce076567cc84923c79732e10526a31df
Mateusz Szefer <szefer.mateusz@gmail.com>
2013-11-20 10:45:35 +0100

    migration dependencies

--- project/apps/multiuploader/migrations/0002_auto__add_field_multiuploaderimage_userprofile__add_field_multiuploade.py
+++ project/apps/multiuploader/migrations/0002_auto__add_field_multiuploaderimage_userprofile__add_field_multiuploade.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("member", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'MultiuploaderImage.userprofile'
         db.add_column(u'multiuploader_multiuploaderimage', 'userprofile',
