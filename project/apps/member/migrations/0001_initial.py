2a55bae72056b5d2234f22cefb68ed75c4097b09
Jakub Wiśniowski <restless.being@gmail.com>
2013-11-20 10:34:04 +0100

    added deps on migrations

--- project/apps/member/migrations/0001_initial.py
+++ project/apps/member/migrations/0001_initial.py
@@ -6,6 +6,10 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (
+        ("event", "0001_initial"),
+        ("multiuploader", "0001_initial"),
+    )
 
     def forwards(self, orm):
         # Adding model 'memberStat'
