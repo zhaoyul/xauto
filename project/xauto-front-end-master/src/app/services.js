534be468d245d46382e0003ff88b347f0e16bd0c
Mateusz Szefer <szefer.mateusz@gmail.com>
2013-11-22 13:08:33 +0100

    Added frontend

---
+++ project/xauto-front-end-master/src/app/services.js
@@ -0,0 +1,9 @@
+angular.module('eventServices', ['ngResource']).
+factory('EventList', function($resource){
+  return $resource('http://localhost:8000/api/events', {}, {
+		getArray: { method: 'GET', isArray: true }
+	});
+}).
+factory('EventObj', function($resource){
+  return $resource('json/obj_event_1.json');
+});
