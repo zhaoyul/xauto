5dae46ce8b89b1dfdc0c1aa2f607efb7fece8d9c
Mateusz Szefer <szefer.mateusz@gmail.com>
2013-12-16 19:57:42 +0100

    Added filter on event list page

--- project/apps/api/views.py
+++ project/apps/api/views.py
@@ -42,10 +42,21 @@ class EventsListView(ListAPIView):
 
     def get_queryset(self):
         search_text = self.request.GET.get('search_text', '')
-        queryset = Event.objects.filter(title__startswith=search_text)
+        filter_by = self.request.GET.get('filter_by', '')
         own_events = self.request.GET.get('own_events', False)
-        if own_events == 'true':
-            queryset = queryset.filter(author=self.request.user.profile)
+        user = self.request.user
+
+        queryset = Event.objects.filter(title__startswith=search_text)
+
+        if own_events == 'true' and user.is_active:
+            queryset = queryset.filter(author=user.profile)
+
+        if filter_by == 'following' and user.is_active:
+            queryset = queryset.filter(followed=user.profile)
+
+        if filter_by == 'live' and user.is_active:
+            queryset = queryset.filter(event_dates__start_date__lt=datetime.now()).filter(
+                event_dates__end_date__gt=datetime.now()).distinct()
         return queryset
 
 
