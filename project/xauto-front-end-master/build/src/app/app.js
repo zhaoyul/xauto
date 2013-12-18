e24ea1de66b287841011ece87c9230f036b74803
Mateusz Szefer <szefer.mateusz@gmail.com>
2013-12-18 10:14:59 +0100

    Added geolocation to Xauto

--- project/xauto-front-end-master/build/src/app/app.js
+++ project/xauto-front-end-master/build/src/app/app.js
@@ -34,7 +34,8 @@ angular.module( 'blvdx', [
 })
 
 .controller( 'AppCtrl', ['$scope', '$state', '$location', 'security', 'Accounts', 'AppScope', '$upload',
-    function AppCtrl ( $scope, $state, $location, security, Accounts, AppScope, $upload ) {
+    function AppCtrl ( $scope, $state, $location, security, Accounts, AppScope, $upload) {
+
     security.requestCurrentUser().then(function (user) {
         $scope.isAuthenticated = security.isAuthenticated;
         $scope.isAdmin = security.isAdmin;
@@ -115,6 +116,45 @@ angular.module( 'blvdx', [
   // ];
 }])
 
+
+.controller( 'GeoCtrl', ['$scope', '$geolocation', function GeoCtrl ( $scope, $geolocation ) {
+    $scope.check = function(){
+        $scope.aviable = $geolocation.aviable;
+        $scope.error = $geolocation.error;
+        $scope.complete = $geolocation.complete;
+        if($geolocation.position){
+            $scope.timestamp = $geolocation.timestamp;
+            $scope.location = $geolocation.position.latitude + " : " + $geolocation.position.longitude;
+        }
+    };
+    $scope.$on(GeolocationEvent.COMPLETE , function (nge){
+        $scope.check();
+        $scope.$apply();// async call z poza angulara potrzebuje apply, inaczej nie zrobi update'u parametrow
+    });
+    $scope.$on(GeolocationEvent.UPDATE , function (nge){
+        $scope.check();
+        $scope.$apply();
+    });
+    $scope.$on(GeolocationEvent.ERROR , function(nge){
+        $scope.check();
+        $scope.$apply();
+    });
+    $scope.single = function(){
+        $geolocation.stopInterval();
+        $geolocation.start();
+    };
+    $scope.continous = function(){
+        if($geolocation.watch == null){
+            $geolocation.startInterval(5000);
+        } else {
+            $geolocation.stopInterval();
+        }
+    };
+    $scope.check();
+}])
+
+
+
 .factory('AppScope', function () {
     var AppScope = {};
 
@@ -127,6 +167,97 @@ angular.module( 'blvdx', [
     };
 
     return AppScope;
+})
+
+.factory("$geolocation", function ($rootScope) {
+    var gloc = {
+        // readed position ::
+        position: null,
+        timestamp: null,
+        // setup ::
+        enableHighAccuracy: true,
+        // current geolocation status ::
+        complete: false,
+        error: false,
+        aviable: false,
+        // continous state ::
+        watch: null,
+        // internal ::
+        maxTimeout: 12000,
+        timeout: null,
+        //
+        startInterval: function (delay, timeout) {
+            if (typeof delay === "undefined") { delay = 10000; }
+            if (typeof timeout === "undefined") { timeout = 15000; }
+            if (navigator && navigator.geolocation) {
+                gloc.watch = navigator.geolocation.watchPosition(function (result) {
+                    console.log(result);
+                    gloc.position = result.coords;
+                    gloc.complete = true;
+                    gloc.timestamp = result.timestamp;
+                    $rootScope.$broadcast(GeolocationEvent.UPDATE, new GeolocationEvent(true, "received"), gloc.position);
+                }, function (result) {
+                    console.log(result);
+                    if (result.code == 3 && gloc.position != null) {
+                        return;
+                    }
+                    gloc.error = true;
+                    $rootScope.$broadcast(GeolocationEvent.ERROR, new GeolocationEvent(false, result.message));
+                }, { enableHighAccuracy: gloc.enableHighAccuracy, timeout: timeout, frequency: delay });
+                return gloc.aviable = true;
+            }
+            return gloc.aviable = false;
+        },
+        stopInterval: function () {
+            if (gloc.watch) {
+                navigator.geolocation.clearWatch(gloc.watch);
+                gloc.watch = null;
+            }
+        },
+        // init call :
+        start: function (useTimeout) {
+            if (typeof useTimeout === "undefined") { useTimeout = true; }
+            if (gloc.timeout != null) {
+                return;
+            }
+            if (navigator && navigator.geolocation != null) {
+                gloc.aviable = true;
+                navigator.geolocation.getCurrentPosition(function (result) {
+                    gloc.abort(false);
+                    gloc.position = result.coords;
+                    gloc.complete = true;
+                    console.log(result);
+                    gloc.timestamp = result.timestamp;
+                    $rootScope.$broadcast(GeolocationEvent.COMPLETE, new GeolocationEvent(true, "complete"), gloc.position);
+                }, function (result) {
+                    gloc.abort(false);
+                    gloc.error = true;
+                    $rootScope.$broadcast(GeolocationEvent.ERROR, new GeolocationEvent(false, result.message));
+                }, { enableHighAccuracy: gloc.enableHighAccuracy });
+                if (useTimeout) {
+                    gloc.timeout = setTimeout(gloc.abort, gloc.maxTimeout);
+                }
+                return;
+            }
+            gloc.aviable = false;
+            gloc.error = true;
+            $rootScope.$broadcast(GeolocationEvent.ERROR);
+        },
+        abort: function (timeEnd) {
+            if (typeof timeEnd === "undefined") { timeEnd = true; }
+            if (gloc.timeout != null) {
+                clearTimeout(gloc.timeout);
+            }
+            if (timeEnd) {
+                gloc.timeout = null;
+                gloc.error = true;
+                $rootScope.$broadcast(GeolocationEvent.ERROR, new GeolocationEvent(false, "Timeout"));
+            }
+        }
+    };
+
+    // return service object ::
+    return gloc;
 })
 
 .directive('bxStreamPhoto', function() {
@@ -159,4 +290,14 @@ angular.module( 'blvdx', [
 //
 
 ;
-
+var GeolocationEvent = (function () {
+    function GeolocationEvent(success, message, position) {
+        this.success = success;
+        this.message = message;
+        this.position = position;
+    }
+    GeolocationEvent.COMPLETE = "geolocation.complete";
+    GeolocationEvent.UPDATE = "geolocation.update";
+    GeolocationEvent.ERROR = "geolocation.error";
+    return GeolocationEvent;
+})();
