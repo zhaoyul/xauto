83b89b83aac1003cc71e5352945d50076d47039e
Jakub Wiśniowski <restless.being@gmail.com>
2014-01-09 23:57:55 +0100

    db path

--- project/production.py
+++ project/production.py
@@ -12,7 +12,7 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.path.dirname(__file__), 'xauto.db'),                      # Or path to database file if using sqlite3.
+        'NAME': '/home/miloweb/www/xauto.dev.milosolutions.com/xauto.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
