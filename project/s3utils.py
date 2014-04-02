23d19dce58f661d25f96c48234d77176569e50e0
Mateusz Szefer <szefer.mateusz@gmail.com>
2014-04-02 08:34:48 +0200

    AWS settings

---
+++ project/s3utils.py
@@ -0,0 +1,7 @@
+from storages.backends.s3boto import S3BotoStorage
+
+#StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+#MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+
+class MediaRootS3BotoStorage(S3BotoStorage):
+    location='media'
