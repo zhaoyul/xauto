9c78f6f4144425f687df36292b801f08d8dd372c
Jakub Wiśniowski <restless.being@gmail.com>
2014-04-24 12:44:47 +0200

    ...

--- project/xauto-front-end-master/build/src/common/security/security.js
+++ project/xauto-front-end-master/build/src/common/security/security.js
@@ -16,13 +16,14 @@ angular.module('security.service', [
   // Login form dialog stuff
   var loginDialog = null;
   function openLoginDialog() {
-//    if ( loginDialog ) {
-//      throw new Error('Trying to open a dialog that is already open!');
-//    }
-//    loginDialog = $dialog.dialog();
-//    loginDialog.open('security/login/form.tpl.html', 'LoginFormController').then(onLoginDialogClose);
-    jQuery.noConflict();
-    $('#loginModal').modal('show');
+      console.log('login dialog');
+    if ( loginDialog ) {
+      throw new Error('Trying to open a dialog that is already open!');
+    }
+    loginDialog = $dialog.dialog();
+    loginDialog.open('security/login/form.tpl.html', 'LoginFormController').then(onLoginDialogClose);
+    //jQuery.noConflict();
+    //$('#loginModal').modal('show');
   }
   function closeLoginDialog(success) {
     if (loginDialog) {
